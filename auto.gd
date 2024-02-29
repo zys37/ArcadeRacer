36c48bee23a0fa2a758062e816075d69cb16a504
zys37 <franeksiek@gmail.com>
2024-02-29 03:42:07 +0100

    deleted npcs, added km meter
    Deleted the npcs and added kilometer counter based on distance beaten.

--- auto.gd
+++ auto.gd
@@ -1,5 +1,7 @@
 extends VehicleBody3D
 const ENGINE_POWER = 200
+var total_distance_meters = 0.0
+var laps_completed: int = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	steering = lerp(steering,Input.get_axis("ui_right","ui_left")*0.4, 5*delta)
@@ -10,4 +12,12 @@ func _process(delta):
 	var speed_meters_per_second = linear_velocity.length()
 	var speed_kph = speed_meters_per_second * 3.6
 	var kph_label:  Label = $"../CanvasLayer/Label"
+	var laps_label: Label = $"../laps/Label"
+	total_distance_meters +=linear_velocity.length()*delta
+	if total_distance_meters>1000:
+		laps_completed+=1
+		total_distance_meters = 0
 	kph_label.text = "SPEED: " + str(round(speed_kph)) + "KPH"
+	laps_label.text = "KILOMETERS " + str(laps_completed)
+func _on_checkpoint_entered():
+	laps_completed+=1
